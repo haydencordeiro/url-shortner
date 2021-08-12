89a2cc2475f2ebe636c02920e8b7056c87e86b73
Hayden Cordeiro <cordeirohayden@gmail.com>
2021-08-12 09:58:20 +0530

    updated links

--- script.py
+++ script.py
@@ -13,7 +13,7 @@ links={
     "yif":"https://drive.google.com/file/d/1qmw0kzT3zaDIO-VbdO80U4PbMT5GvM9m/view?usp=sharing",
     "teknack2021":"https://drive.google.com/file/d/1M5l1H28-eCANaCmfDHKw_zz9jKE_1fPS/view?usp=sharing",
     "teknack2020":"https://drive.google.com/file/d/1_h0LNVKDclN4B73DllhZqRlVS_iTnE20/view?usp=sharing",
-    "dsip":"https://meet.google.com/bvd-ockm-bxq",
+    "dsip":"https://meet.google.com/obk-kjzx-ago",
     "assdf":"https://us02web.zoom.us/j/7487113416?pwd=NXdUYVpXdGdnY3lMbkhHZjFyYm5vZz09",
     "assdfrec":"https://mega.nz/folder/bKBEBDKR#7-I6nZdMl9xzzjkYrZVH3g",
     "zoom":"https://us04web.zoom.us/j/7661382898?pwd=Z01xa05kcE9OR2ZVKzFEUDZZWWFXZz09",
