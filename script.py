a88111cb40d2fe0f7ccb67dcb90d7c38478467f2
Hayden Cordeiro <cordeirohayden@gmail.com>
2023-08-03 16:38:19 +0530

    cleanup

--- script.py
+++ script.py
@@ -10,8 +10,6 @@ links={
     "marksheets":"https://drive.google.com/file/d/1A1NP0Xd0sWpnIBZtvLbMHG9M9Esxy-Kn/view?usp=sharing",
     "aadharpanbirth":"https://drive.google.com/file/d/1sz1-alDLg6w6JeCylwSFR_MT5xDyrVEd/view?usp=sharing",
     "assdfrec":"https://mega.nz/folder/bKBEBDKR#7-I6nZdMl9xzzjkYrZVH3g",
-    "sem7":"https://mega.nz/folder/PawUALSY#XudA6PFfm34RqRYavnz-yg",
-    "sem72":"https://drive.google.com/drive/folders/1MkhH6xJ3ug_Fo7cKsFL-WECCBAVhgEVQ",
     "zenscape":"https://play.google.com/store/apps/details?id=com.Teknack.Zenscape&hl=en_IN&gl=US",
     "gre":"https://mega.nz/folder/6bRCVZyK#fJ0wI9IUUP2p3Ypgtsmcdg",
     "ielts":"https://mega.nz/folder/DfIgkDLA#7st_w2l6QKSiyj8m1Zi6JQ",
@@ -19,10 +17,6 @@ links={
     "zoom":"https://us04web.zoom.us/j/7661382898?pwd=Z01xa05kcE9OR2ZVKzFEUDZZWWFXZz09",
     "ruvin":"https://meet.google.com/jbz-agyh-oym",
     "jivin":"https://meet.google.com/kgg-nyxj-gyb",
-    "comps":"https://www.appsheet.com/start/c3eb0069-1b16-4590-8b4b-68f06960fde1",
-    "dc":"https://docs.google.com/document/d/1ramBKCWW8Y8RnIO57yMoF8qQq2JxUhGyqE4_GW7E684/edit?usp=drivesdk",
-    "transmission":"http://192.168.0.109:9091/transmission/web/",
-    "nas":"http://192.168.0.102/ui/plugins",
     "whatsapp": "https://wa.me/917039964659"
 
 }
